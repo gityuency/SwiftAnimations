baacf128e5773681fbb4234fda8bd3d32bf58596
yuency yang <ext.yuency.yang@ef.com>
2018-01-08 09:47:42 +0800

    修改了随机颜色的方法

--- Animations/Tools/UIColor+Extension.swift
+++ Animations/Tools/UIColor+Extension.swift
@@ -12,14 +12,10 @@ import UIKit
 // MARK: - UIColor 扩展
 extension UIColor {
     
-    /// 获得随机颜色  UIColor().randomColor
-    var randomColor: UIColor {
-        get{
-            return UIColor(red: CGFloat(arc4random_uniform(256)) / 255.0, green: CGFloat(arc4random_uniform(256)) / 255.0, blue: CGFloat(arc4random_uniform(256)) / 255.0, alpha: 1)
-        }
+    /// 获得随机颜色  UIColor.randomColor()
+    static func randomColor() -> UIColor {
+        return UIColor(red: CGFloat(arc4random_uniform(256)) / 255.0, green: CGFloat(arc4random_uniform(256)) / 255.0, blue: CGFloat(arc4random_uniform(256)) / 255.0, alpha: 1)
     }
- 
-
     
     /// 用数值初始化颜色，便于生成设计图上标明的十六进制颜色  UIColor(valueRGB: 0x666666, alpha: 1.0)
     convenience init(valueRGB: UInt, alpha: CGFloat = 1.0) {
